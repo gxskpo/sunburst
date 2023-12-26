0e454ab8297e83f69b462da64ee53a5f861f3fa0
haru <ggalactiko@galactiko.net>
2023-12-26 16:24:55 -0600

    Rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import {SpeedInsights} from "@vercel/speed-insights/next"
 import {NotificationWidget} from "@/app/nfWidget";
 
 export const metadata: Metadata = {
-    title: 'LunaBurst',
+    title: 'SunBurst',
+    description: 'SunBurst, el mejor bot de discord en español',
 }
 
 export default function RootLayout({
@@ -19,6 +20,11 @@ export default function RootLayout({
             <title>{metadata.title}</title>
             {/* @ts-ignore */}
             <meta name="description" content={metadata.description}/>
+            <meta property="og:title" content="SunBurst | Bot de Discord"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:description" content="SunBurts, el mejor bot de discord en español"/>
+            <meta property="og:image" content="/sunburst-banner.png"/>
+            <meta name="theme-color" content="#5d35a4"/>
             <script src={"https://kit.fontawesome.com/8e4c18f6c2.js"} crossOrigin="anonymous"></script>
             <script defer src="https://overtracking.com/p/KXWaXiao8mO7ovqA"></script>
         </head>
